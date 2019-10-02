de4ec834a63edb64f63a9512e40b316220810f44
Nicolas Paes Leme <nicolaspleme@gmail.com>
2019-10-02 18:33:32 -0300

    Parsing the .json correctly and aux functions

--- Program.cs
+++ Program.cs
@@ -9,22 +9,42 @@ namespace TPS_IA
         static void Main(string[] args)
         {
             ParseTPS parser = new ParseTPS(); 
-            tspclass gr17;
+            tspclass tspFile = null;
             string fileName;
+
+            Console.WriteLine("Please enter the name of the .tsp file");
 
             fileName = Console.ReadLine();
             Console.WriteLine("fileName:  " + fileName);
             Console.ReadKey();
 
-            gr17 = JsonConvert.DeserializeObject<tspclass>(File.ReadAllText(@"tspjson/"+ fileName + ".json"));
+            bool fileExists = File.Exists(@"tspjson/" + fileName + ".json");
+            if(fileExists) tspFile = JsonConvert.DeserializeObject<tspclass>(File.ReadAllText(@"tspjson/"+ fileName + ".json"));
+            else Console.WriteLine("File do not exist");
 
+            Console.WriteLine("OptDistance  :" + tspFile.OptDistance);
+            Console.WriteLine("OptTour  :" + tspFile.OptTour.Length);
+            Console.WriteLine("Format  :" + tspFile.Format);
 
-            Console.WriteLine("PARSE TEST  :" + gr17.OptDistance);
-            Console.WriteLine("OptTour  :" + gr17.OptTour.Length);
-
-            foreach (int point in gr17.OptTour)
+            foreach (int point in tspFile.OptTour)
             {
                 Console.Write("  " + point);
+            }
+            Console.WriteLine("\nDistance Matrix");
+            foreach (int[] row in tspFile.DistanceMatrix)
+            {
+                foreach (int i in row)
+                {
+                    Console.Write("  " + i);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("TEST DISTANCE CITY\n");
+            //TEST CITY A
+            for (int i = 0; i < tspFile.Dimension; i++)
+            {
+                Console.Write("  " + tspFile.GetDistance(1,i));
             }
         }
     }
--- tspclass.cs
+++ tspclass.cs
@@ -4,15 +4,43 @@ namespace TPS_IA
 {
     public class tspclass
     {
+        public enum EdgeFormat
+        {
+            FULL_MATRIX,
+            UPPER_ROW,
+            LOWER_ROW,
+            UPPER_DIAG_ROW,
+            LOWER_DIAG_ROW,
+            UPPER_COL,
+            LOWER_COL,
+            UPPER_DIAG_COL,
+            LOWER_DIAG_COL
+        }
+        public EdgeFormat Format;
+        public int Dimension;
         public int TourSize;
         public int[] OptTour;
         public int OptDistance;
+        public int[][] DistanceMatrix;
 
         public tspclass(int tourSize, int[] optTour, int optDistance)
         {
             TourSize = tourSize;
             OptTour = optTour;
             OptDistance = optDistance;
+        }
+
+        public float GetDistance(int cityA, int cityB)
+        {
+            if(this.Format.Equals(EdgeFormat.LOWER_DIAG_ROW))
+            {
+                if(cityA < this.Dimension || cityB < this.Dimension)
+                {
+                    return this.DistanceMatrix[cityA][cityB];
+                    
+                }
+            }
+            return -1; //Return -1 if the cities asked are out of bounds;
         }
     }
 }
